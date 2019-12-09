3d3675aba17c28e88ff37ae4dab89062801c382f
Christian Anderson <ca96187@gmail.com>
2019-12-09 16:42:17 -0700

    created base code for server index file

---
+++ server/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const massive = require('massive');
+const session = require('express-session');
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env;
+
+// -- MIDDLEWARE -- //
+
+// give ability to parse incoming JSON from req.body
+app.use(express.json());
+
+// cookie
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 2 // 2 hours
+  }
+}));
+
+// -- MASSIVE -- //
+massive(CONNECTION_STRING).then((database) => {
+  app.set("db", database);
+  console.log('database set!');
+  console.log(database.listTables());
+  app.listen(SERVER_PORT, () => {
+    console.log(`listening on port ${SERVER_PORT}`);
+  });
+});
+
+// -- ENDPOINTS -- //
+
+// Authentication
+
+
+// User Saved Links
