8f964e631a6cb3c1f2f547ece1b852ecc18275f2
Christian Anderson <ca96187@gmail.com>
2019-12-10 09:43:58 -0700

    started building Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+  componentDidMount() {
+    window.scrollTo(0, 0)
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Home;
