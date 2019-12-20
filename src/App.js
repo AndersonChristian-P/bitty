f675e9cb8e7f5a6fb7280b3a8e56d9533fb8ac6b
Christian Anderson <ca96187@gmail.com>
2019-12-20 16:17:26 -0700

    created Header and Footer components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { HashRouter } from 'react-router-dom';
 
 import router from './router';
 
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
 // import './App.scss';
 
 function App() {
   return (
     <HashRouter>
+      <Header />
       {router}
+      <Footer />
     </HashRouter>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      FOOTER
+    </div>
+  );
+}
+
+// Footer doesn't have a state, so made the component function based
+
+export default Footer;
