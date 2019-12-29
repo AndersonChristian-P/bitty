eb426570be661b587639ed6b7d884d6e0b5c90a0
Christian Anderson <ca96187@gmail.com>
2019-12-28 19:36:47 -0700

    coded the basic structure and styling for the header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
 
@@ -10,8 +11,24 @@ class Header extends Component {
 
   render() {
     return (
-      <div>
-        HEADER
+      <div className="header">
+        <span className="logo-pages">
+          <figure className="logo"> bitty
+          {/* <img src="" alt=""/> */}
+          </figure>
+          <nav className="pages">
+            <Link className="page" to="/whyBitty">why bitty?</Link>
+            <Link className="page" to="/solutions">solutions</Link>
+            <Link className="page" to="/features">features</Link>
+            <Link className="page" to="/pricing">pricing</Link>
+            <Link className="page" to="/support">support</Link>
+          </nav>
+        </span>
+
+        <span className="login-register">
+          <span className="login">log in</span>
+          <span className="register">sign up</span>
+        </span>
       </div>
     );
   }
