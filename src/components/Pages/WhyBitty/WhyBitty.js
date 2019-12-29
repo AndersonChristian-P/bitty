49112958bdf5463d4882782d7ce22617ec8caa44
Christian Anderson <ca96187@gmail.com>
2019-12-28 19:35:13 -0700

    created components under Pages and included them in router

---
+++ src/components/Pages/WhyBitty/WhyBitty.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WhyBitty = () => {
+  return (
+    <div>
+      Why Bitty?
+    </div>
+  )
+}
+
+export default WhyBitty;
--- src/router.js
+++ src/router.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
+import Features from "./components/Pages/Features/Features";
+import Pricing from "./components/Pages/Pricing/Pricing";
+import Solutions from "./components/Pages/Solutions/Solutions";
+import Support from "./components/Pages/Support/Support";
+import WhyBitty from "./components/Pages/WhyBitty/WhyBitty";
 
 export default (
   <Switch>
     <Route exact path='/' component={Home} />
+    <Route path="/features" component={Features} />
+    <Route path="/pricing" component={Pricing} />
+    <Route path="/solutions" component={Solutions} />
+    <Route path="/support" component={Support} />
+    <Route path="/whyBitty" component={WhyBitty} />
   </Switch>
 )
