005dc5b924e1643a3f01f09f05ba885c49c2759f
Christian Anderson <ca96187@gmail.com>
2020-01-01 10:42:47 -0700

    used semantic element header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,25 +11,27 @@ class Header extends Component {
 
   render() {
     return (
-      <div className="header">
-        <span className="logo-pages">
-          <figure className="logo"> bitty
+      <>
+        <header>
+          <span className="logo-pages">
+            <figure className="logo"> bitty
           {/* <img src="" alt=""/> */}
-          </figure>
-          <nav className="pages">
-            <Link className="page" to="/whyBitty">why bitty?</Link>
-            <Link className="page" to="/solutions">solutions</Link>
-            <Link className="page" to="/features">features</Link>
-            <Link className="page" to="/pricing">pricing</Link>
-            <Link className="page" to="/support">support</Link>
-          </nav>
-        </span>
+            </figure>
+            <nav className="pages">
+              <Link className="page" to="/whyBitty">why bitty?</Link>
+              <Link className="page" to="/solutions">solutions</Link>
+              <Link className="page" to="/features">features</Link>
+              <Link className="page" to="/pricing">pricing</Link>
+              <Link className="page" to="/support">support</Link>
+            </nav>
+          </span>
 
-        <span className="login-register">
-          <span className="login">log in</span>
-          <span className="register">sign up</span>
-        </span>
-      </div>
+          <span className="login-register">
+            <span className="login">log in</span>
+            <span className="register">sign up</span>
+          </span>
+        </header>
+      </>
     );
   }
 
