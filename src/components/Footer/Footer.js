8c5645af37e7c112fc3ede6da7cf1c03f5410441
Christian Anderson <ca96187@gmail.com>
2020-01-01 10:42:02 -0700

    started code for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,25 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      FOOTER
-    </div>
+    <>
+      <footer>
+
+        <div className="newsletter">
+          <h3>Sign up for our newsletter</h3>
+
+          <div className="newsletter-input">
+            placeholder for email input
+            <button>
+              Submit
+            </button>
+          </div>
+
+          <p>It will inspire you.</p>
+
+        </div>
+
+      </footer>
+    </>
   );
 }
 
